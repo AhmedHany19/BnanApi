fd97dff012c5f3c7fda5c5c506fd22ccac1eee9f
unknown <ahmedhanyzaher19@gmail.com>
2024-04-18 14:54:50 +0200

    FirstCommit

---
+++ BnanApi/Controllers/CarsController.cs
@@ -0,0 +1,96 @@
+﻿using BnanApi.Models;
+using BnanApi.DTOS;
+using BnanApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using Microsoft.AspNetCore.Identity;
+
+namespace BnanApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarsController : Controller
+    {
+        private readonly BnanKSAContext _context;
+
+        public CarsController(BnanKSAContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCar()
+        {
+            //var carsAvailable = _unitOfWork.CrCasCarInformation.FindAll(x => x.CrCasCarInformationLessor == lessorCode && x.CrCasCarInformationBranch == bSLayoutVM.SelectedBranch && x.CrCasCarInformationStatus == Status.Active &&
+            //                                                                     x.CrCasCarInformationPriceStatus == true && x.CrCasCarInformationOwnerStatus == Status.Active &&
+            //                                                                    (x.CrCasCarInformationForSaleStatus == Status.Active || x.CrCasCarInformationForSaleStatus == Status.RendAndForSale),
+            //                                                                    new[] { "CrCasCarInformationDistributionNavigation", "CrCasCarInformationDistributionNavigation.CrCasPriceCarBasics",
+            //                                                                            "CrCasCarInformationCategoryNavigation", "CrCasCarDocumentsMaintenances","CrCasCarInformationCvtNavigation",
+            //                                                                            "CrCasCarInformationFuelNavigation" }).ToList();
+            List<CarInfomationDTO> carInfomationDTOList = new List<CarInfomationDTO>();
+            var cars = await _context.CrCasCarInformations.Where(x => x.CrCasCarInformationForSaleStatus != "A").Include(x=>x.CrCasCarInformationDistributionNavigation).ToListAsync();
+            foreach (var car in cars)
+            {
+                var lessorInfo = await _context.CrMasLessorInformations.FirstOrDefaultAsync(x => x.CrMasLessorInformationCode == car.CrCasCarInformationLessor);
+
+                CarInfomationDTO carInfomationDTO = new CarInfomationDTO();
+                carInfomationDTO.CarArName= car.CrCasCarInformationConcatenateArName;
+                carInfomationDTO.CarEnName= car.CrCasCarInformationConcatenateEnName;
+                carInfomationDTO.CompanyArName= lessorInfo.CrMasLessorInformationArLongName;
+                carInfomationDTO.CompanyEnName= lessorInfo.CrMasLessorInformationEnLongName;
+                carInfomationDTO.CurrentMeter= car.CrCasCarInformationCurrentMeter?.ToString("N0",CultureInfo.InvariantCulture);
+                carInfomationDTO.TelephoneContact= lessorInfo.CrMasLessorInformationCommunicationMobileKey + lessorInfo.CrMasLessorInformationCallFree;
+                carInfomationDTO.Email= lessorInfo.CrMasLessorInformationEmail;
+                carInfomationDTO.ImagePath= car.CrCasCarInformationDistributionNavigation.CrMasSupCarDistributionImage;
+                carInfomationDTO.Price = car.CrCasCarInformationOfferValueSale?.ToString("N2", CultureInfo.InvariantCulture);
+                carInfomationDTOList.Add(carInfomationDTO);
+            }
+            return Ok(carInfomationDTOList);
+        }
+        [HttpGet("Categories")]
+        public async Task<IActionResult> GetCategories()
+        {
+            
+            List<CategoriesDTO> CategoriesDTOList = new List<CategoriesDTO>();
+            var cars = await _context.CrCasCarInformations.Where(x => x.CrCasCarInformationForSaleStatus != "A").Include(x => x.CrCasCarInformationDistributionNavigation).Include(x=>x.CrCasCarInformationCategoryNavigation).ToListAsync();
+            var categoryCars = cars.Select(item => item.CrCasCarInformationCategoryNavigation).Distinct().OrderBy(item => item.CrMasSupCarCategoryCode).ToList();
+            foreach (var categoryCar in categoryCars)
+            {
+                CategoriesDTO CategoriesDTO = new CategoriesDTO();
+                CategoriesDTO.Code = categoryCar.CrMasSupCarCategoryCode;
+                CategoriesDTO.ArName = categoryCar.CrMasSupCarCategoryArName;
+                CategoriesDTO.EnName = categoryCar.CrMasSupCarCategoryEnName;
+                CategoriesDTOList.Add(CategoriesDTO);
+
+            }
+
+            return Ok(CategoriesDTOList);
+        }
+        [HttpGet("GetCarsByCategory")]
+        public async Task<IActionResult> GetCarBysCategory(string Code)
+        {
+
+            List<CarInfomationDTO> carInfomationDTOList = new List<CarInfomationDTO>();
+            var cars = new List<CrCasCarInformation>();
+            if (Code == "3400000000") cars = await _context.CrCasCarInformations.Where(x => x.CrCasCarInformationForSaleStatus != "A").Include(x => x.CrCasCarInformationDistributionNavigation).ToListAsync();
+            else cars = await _context.CrCasCarInformations.Where(x => x.CrCasCarInformationForSaleStatus != "A" && x.CrCasCarInformationCategory == Code).Include(x => x.CrCasCarInformationDistributionNavigation).ToListAsync();
+            foreach (var car in cars)
+            {
+                var lessorInfo = await _context.CrMasLessorInformations.FirstOrDefaultAsync(x => x.CrMasLessorInformationCode == car.CrCasCarInformationLessor);
+
+                CarInfomationDTO carInfomationDTO = new CarInfomationDTO();
+                carInfomationDTO.CarArName = car.CrCasCarInformationConcatenateArName;
+                carInfomationDTO.CarEnName = car.CrCasCarInformationConcatenateEnName;
+                carInfomationDTO.CompanyArName = lessorInfo.CrMasLessorInformationArLongName;
+                carInfomationDTO.CompanyEnName = lessorInfo.CrMasLessorInformationEnLongName;
+                carInfomationDTO.CurrentMeter = car.CrCasCarInformationCurrentMeter?.ToString("N0", CultureInfo.InvariantCulture);
+                carInfomationDTO.TelephoneContact = lessorInfo.CrMasLessorInformationCommunicationMobileKey + lessorInfo.CrMasLessorInformationCallFree;
+                carInfomationDTO.Email = lessorInfo.CrMasLessorInformationEmail;
+                carInfomationDTO.ImagePath = car.CrCasCarInformationDistributionNavigation.CrMasSupCarDistributionImage;
+                carInfomationDTO.Price = car.CrCasCarInformationOfferValueSale?.ToString("N2", CultureInfo.InvariantCulture);
+                carInfomationDTOList.Add(carInfomationDTO);
+            }
+            return Ok(carInfomationDTOList);
+        }
+    }
+}
---
+++ BnanApi/Models/CrMasLessorImage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BnanApi.Models
+{
+    public partial class CrMasLessorImage
+    {
+        public string CrMasLessorImageCode { get; set; } = null!;
+        public string? CrMasLessorImageLogo { get; set; }
+        public string? CrMasLessorImageStamp { get; set; }
+        public string? CrMasLessorImageStampOutsideCity { get; set; }
+        public string? CrMasLessorImageStampOutsideCountry { get; set; }
+        public string? CrMasLessorImageStampFullAmountPaid { get; set; }
+        public string? CrMasLessorImageSignatureDirector { get; set; }
+        public string? CrMasLessorImageContractCard { get; set; }
+        public string? CrMasLessorImageContractExtensionCard { get; set; }
+        public string? CrMasLessorImageContract24Hour { get; set; }
+        public string? CrMasLessorImageContract4Hour { get; set; }
+        public string? CrMasLessorImageContractFinished { get; set; }
+        public string? CrMasLessorImageContractClosed { get; set; }
+        public string? CrMasLessorImageContractCancelled { get; set; }
+        public string? CrMasLessorImageArCatchReceipt { get; set; }
+        public string? CrMasLessorImageArBillExchange { get; set; }
+        public string? CrMasLessorImageArBoxFeeding { get; set; }
+        public string? CrMasLessorImageArDeliveryCustody { get; set; }
+        public string? CrMasLessorImageArTransferFtenant { get; set; }
+        public string? CrMasLessorImageArTransferTtenant { get; set; }
+        public string? CrMasLessorImageEnCatchReceipt { get; set; }
+        public string? CrMasLessorImageEnBillExchange { get; set; }
+        public string? CrMasLessorImageEnBoxFeeding { get; set; }
+        public string? CrMasLessorImageEnDeliveryCustody { get; set; }
+        public string? CrMasLessorImageEnTransferFtenant { get; set; }
+        public string? CrMasLessorImageEnTransferTtenant { get; set; }
+        public string? CrMasLessorImageArContractPage1 { get; set; }
+        public string? CrMasLessorImageArContractPage2 { get; set; }
+        public string? CrMasLessorImageArContractPage3 { get; set; }
+        public string? CrMasLessorImageArContractPage4 { get; set; }
+        public string? CrMasLessorImageArContractPage5 { get; set; }
+        public string? CrMasLessorImageArContractPage6 { get; set; }
+        public string? CrMasLessorImageArContractPage7 { get; set; }
+        public string? CrMasLessorImageArContractPage8 { get; set; }
+        public string? CrMasLessorImageArContractPage9 { get; set; }
+        public string? CrMasLessorImageEnContractPage1 { get; set; }
+        public string? CrMasLessorImageEnContractPage2 { get; set; }
+        public string? CrMasLessorImageEnContractPage3 { get; set; }
+        public string? CrMasLessorImageEnContractPage4 { get; set; }
+        public string? CrMasLessorImageEnContractPage5 { get; set; }
+        public string? CrMasLessorImageEnContractPage6 { get; set; }
+        public string? CrMasLessorImageEnContractPage7 { get; set; }
+        public string? CrMasLessorImageEnContractPage8 { get; set; }
+        public string? CrMasLessorImageEnContractPage9 { get; set; }
+
+        public virtual CrMasLessorInformation CrMasLessorImageCodeNavigation { get; set; } = null!;
+    }
+}
